c5e5d4e57c0d1d316bd432dded511239b7974807
Firsov Anton <kamikadze1887@gmail.com>
2021-12-06 11:56:48 +0400

    show basket if empty, btn basket

--- src/main/resources/static/js/add_basket.js
+++ src/main/resources/static/js/add_basket.js
@@ -33,22 +33,70 @@ checkBasket();
 
 function showMiniBasket(){
     //отображение содержимого корзины
-    var out ='Товары в корзине:<br/>';
+    var out = "";
     for (var item in basket){
-        out +=  basket[item].title + ' --- ' + basket[item].count+'<br/>';
+        out += '<b>' + basket[item].title + ' </b>: ' + basket[item].count+'<br/>';
     }
-    $('#mini-basket').html(out);
+    $('#basket_content').html(out);
 }
 showMiniBasket();
 
 
 $(document).ready(function(){
-    //отображение содержимого корзины
-    var out ='Товары в корзине:<br/>';
-    for (var item in basket){
-        out +=  '<div class="alert alert-info mt-2 col-md-8 offset-md-2 py-5">' + '<button className="delete">x</button>' + basket[item].title + basket[item].picture + '<button class="minus">-</button>' + basket[item].count + '<button class="plus">+</button>' + basket[item].price + basket[item].count + '</div>';
+    if ( $.isEmptyObject(basket) ){
+        //корзина пуста
+        var out = '<br> <h3>Корзина пуста. Добавьте товар в корзину</h3> <br> <div class="d-grid gap-2"> <a class="btn btn-info" href="/" role="button">Каталог</a> </div>';
         $('#my-basket').html(out);
+    }
+    else {
+    //отображение содержимого корзины
+    var out ='<h1>Товары в корзине:</h1><br/>';
+    for (var item in basket){
+        out +=  '<div class="alert alert-info mt-2 col-md-8 offset-md-2 py-5">' +
+            '<button class="delete btn btn-outline-dark" data-art"'+item+'">x</button>' +
+            basket[item].title +
+            basket[item].picture +
+            '<button class="minus btn btn-danger" data-art"'+item+'">-</button>' +
+            basket[item].count +
+            '<button class="plus btn btn-success" data-art"'+item+'">+</button>' +
+            basket[item].price +
+            basket[item].count +
+            '</div>';
+        $('#my-basket').html(out);
+    }
+    $('.plus').on('click', plusTovar);
+    $('.minus').on('click', minusTovar);
+    $('.delete').on('click', deleteTovar);
+    }
+    function plusTovar(){
+        var item = $(this).attr('data-art');
+        basket[item].count++;
+        saveBasketToLS();
+        showMiniBasket();
+    }
+
+    function minusTovar(){
+        var item = $(this).attr('data-art');
+        if (basket[item]>1) {
+            basket[item].count--;
+        }
+        else {
+            delete basket[item];
+        }
+        saveBasketToLS();
+        showMiniBasket();
+    }
+
+    function deleteTovar(){
+        var item = $(this).attr('data-art');
+        delete basket[item];
+        saveBasketToLS();
+        showMiniBasket();
     }
 
 });
+
+function saveBasketToLS(){
+    localStorage.setItem('basket', JSON.stringify(basket));
+}
 
